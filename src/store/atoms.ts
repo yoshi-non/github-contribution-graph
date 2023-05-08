241a945931965d01d0a52187427cca27e60599d0
yoshi-non <himekawa.yoshino44@gmail.com>
2023-05-08 15:53:04 +0900

    初期環境構築

---
+++ src/store/atoms.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+// export const textState = atom<string>({
+//   key: 'textState', // unique ID (with respect to other atoms/selectors)
+//   default: 'Hello World!', // default value (aka initial value)
+// });
