199b3ec93a281689050260e82d88bc64b30021d3
yoshi-non <himekawa.yoshino44@gmail.com>
2023-05-10 17:18:50 +0900

    githubのapiからuserのicon画像とユーザー名を取得

--- src/githubUserList.ts
+++ src/githubUserList.ts
@@ -1,52 +1,53 @@
-export const githubUserList = [
+export const defaultGithubUserList = [
   {
     id: 'yoshi-non',
-    name: 'Kagari',
+    name: '',
+    avatarUrl: '',
+    allContributions: 0,
     color: '#ff0000',
   },
   {
     id: 'lovecoal0928',
-    name: 'Coa3',
+    name: '',
+    avatarUrl: '',
+    allContributions: 0,
     color: '#000000',
   },
   {
-    id: 'Frogman317',
-    name: 'Mr.Frogman',
-    color: '#00ff00',
-  },
-  {
-    id: 'bearkaito',
-    name: 'bearkaito',
-    color: '#00ced1',
-  },
-  {
     id: 'Hiroto2002',
-    name: 'Hiroto2002',
+    name: '',
+    avatarUrl: '',
+    allContributions: 0,
     color: '#2f4f4f',
   },
-  {
-    id: 'd4n1938',
-    name: 'Lnida',
-    color: '#006400',
-  },
-  {
-    id: 'minntoko',
-    name: 'みんとこ',
-    color: '#ffff00',
-  },
-  {
-    id: 'aijkl',
-    name: 'Aijkl',
-    color: '#ffa500',
-  },
-  {
-    id: 'NewNomad',
-    name: 'NewNomad',
-    color: '#000080',
-  },
-  {
-    id: 'mitsuYashi',
-    name: 'mitsuki',
-    color: '#9400d3',
-  },
+  // {
+  //   id: 'bearkaito',
+  //   name: 'bearkaito',
+  //   color: '#00ced1',
+  // },
+  // {
+  //   id: 'd4n1938',
+  //   name: 'Lnida',
+  //   color: '#006400',
+  // },
+  // {
+  //   id: 'minntoko',
+  //   name: 'みんとこ',
+  //   color: '#ffff00',
+  // },
+  // {
+  //   id: 'aijkl',
+  //   name: 'Aijkl',
+  //   color: '#ffa500',
+  // },
+  // {
+  //   id: 'NewNomad',
+  //   name: 'NewNomad',
+  //   color: '#000080',
+  // },
+  // {
+  //   id: 'mitsuYashi',
+  //   name: 'mitsuki',
+  //   color: '#9400d3',
+  // },
 ];
