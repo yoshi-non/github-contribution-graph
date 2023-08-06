c0f37a71f19e9bc53ddd4e3d9d640b5edf709c4a
yoshi-non <himekawa.yoshino44@gmail.com>
2023-08-06 17:22:50 +0900

    feat ログインを非同期で行う

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,25 +1,7 @@
-import { auth } from '@/lib/firebaseClient';
-import {
-  GithubAuthProvider,
-  signInWithPopup,
-  signOut,
-} from 'firebase/auth';
+import { login, logout } from '@/lib/auth';
 import React from 'react';
 
 const LoginPage = () => {
-  const login = () => {
-    const provider = new GithubAuthProvider();
-    signInWithPopup(auth, provider).then((result) => {
-      alert('ログインしました');
-      console.log(result.user);
-    });
-  };
-  const logout = () => {
-    signOut(auth).then(() => {
-      alert('ログアウトしました');
-    });
-  };
-
   return (
     <div>
       <h1>Login Page</h1>
