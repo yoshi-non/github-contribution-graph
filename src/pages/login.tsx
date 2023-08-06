d86dc90e678b147a1d95b287341505969be379bc
yoshi-non <himekawa.yoshino44@gmail.com>
2023-08-06 17:12:41 +0900

    feat ログアウト追加

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,41 +2,29 @@ import { auth } from '@/lib/firebaseClient';
 import {
   GithubAuthProvider,
   signInWithPopup,
+  signOut,
 } from 'firebase/auth';
 import React from 'react';
 
 const LoginPage = () => {
   const login = () => {
     const provider = new GithubAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        const credential =
-          GithubAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-
-        // The signed-in user info.
-        const user = result.user;
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
-        console.log(user);
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential =
-          GithubAuthProvider.credentialFromError(error);
-        // ...
-      });
+    signInWithPopup(auth, provider).then((result) => {
+      alert('ログインしました');
+      console.log(result.user);
+    });
   };
+  const logout = () => {
+    signOut(auth).then(() => {
+      alert('ログアウトしました');
+    });
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
       <button onClick={() => login()}>ログイン</button>
+      <button onClick={() => logout()}>ログアウト</button>
     </div>
   );
 };
